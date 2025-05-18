c85a2c4e6920d6064ac1c2c4f1842d4929a75c6a
gtapiero <giovanny.tapiero@pi.edu.co>
2025-05-18 16:35:09 -0500

    commit 2

--- EscuelaMVC/Controllers/EstudianteController.cs
+++ EscuelaMVC/Controllers/EstudianteController.cs
@@ -1,6 +1,42 @@
-﻿namespace EscuelaMVC.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using EscuelaMVC.Models;
+using EscuelaMVC.DAO;
+
+namespace EscuelaMVC.Controllers
 {
-    public class EstudianteController
+    public class EstudianteController : Controller
     {
+        private readonly EstudianteDAO dao;
+
+        public EstudianteController(IConfiguration config)
+        {
+            dao = new EstudianteDAO(config);
+        }
+
+        // GET: /Estudiante/
+        public IActionResult Index()
+        {
+            var lista = dao.ObtenerTodos();
+            return View(lista);
+        }
+
+        // GET: /Estudiante/Crear
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+/*        // POST : /Estudiante/Crear
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                dao.Insertar(estudiante);
+                return RedirectToAction("Index");
+            }
+        }*/
     }
 }
